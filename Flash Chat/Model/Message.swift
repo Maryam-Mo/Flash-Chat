be4d883b889400e355191dad44374b8244875019
Maryam <Maryam_moein@hotmail.com>
2020-05-21 10:30:44 +0800

    implement flash chat project

---
+++ Flash Chat/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  Created by Maryam on 5/20/20.
+//  Copyright © 2020 Sofftech. All rights reserved.
+//
+
+class Message {
+    
+    var sender : String = ""
+    var messageBody : String = ""
+}
---
+++ Flash Chat/View/Custom Cell/CustomMessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomMessageCell.swift
+//  Flash Chat
+//
+//  Created by Maryam on 5/20/20.
+//  Copyright © 2020 Sofftech. All rights reserved.
+//
+
+import UIKit
+
+class CustomMessageCell: UITableViewCell {
+
+
+    @IBOutlet var messageBackground: UIView!
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var messageBody: UILabel!
+    @IBOutlet var senderUsername: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code goes here
+        
+        
+        
+    }
+
+
+}
